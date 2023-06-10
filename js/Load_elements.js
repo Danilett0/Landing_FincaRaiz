14b8151fed90f9e765d833d3a36c8975127393b3
Danilett0 <daniilo.97@hotmail.com>
2023-06-09 23:30:39 -0500

    ajusto ruta

--- js/Load_elements.js
+++ js/Load_elements.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
-  loadContent(document.querySelector("head"), "/xxx/components/head.html");
-  loadContent(document.querySelector("header"), "xx/components/header.html");
-  loadContent(document.querySelector("footer"), "xx/components/footer.html");
+  loadContent(document.querySelector("head"), "/components/head.html");
+  loadContent(document.querySelector("header"), "/components/header.html");
+  loadContent(document.querySelector("footer"), "/components/footer.html");
 });
 
 function loadContent(element, file) {
